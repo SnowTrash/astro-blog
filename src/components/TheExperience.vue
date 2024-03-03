84b39b719aa9f08c70089a421de9297c8343d53f
SnowTrash <juan.vargas2962@alumnos.udg.mx>
2024-03-02 19:05:16 -0600

    hw-Tresjs
    Hola mundo con TresJs , se crea una escena con un objeto 3D mediante Vue

---
+++ src/components/TheExperience.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+
+import { TresCanvas } from '@tresjs/core'
+
+import { BasicShadowMap, SRGBColorSpace, NoToneMapping } from 'three'
+
+import { OrbitControls } from '@tresjs/cientos'
+
+const gl = {
+  clearColor: '#181C3E',
+  shadows: true,
+  alpha: false,
+  shadowMapType: BasicShadowMap,
+  outputColorSpace: SRGBColorSpace,
+  toneMapping: NoToneMapping,
+}
+
+</script>
+
+<template>
+  <div class="tres-container">
+    <TresCanvas v-bind="gl">
+
+    <TresPerspectiveCamera
+      :position="[3, 3, 3]"
+      :fov="45"
+      :look-at="[0, 0, 0]"
+    />
+
+      <TresMesh>
+        <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
+        <TresMeshBasicMaterial color="orange" />
+      </TresMesh>
+      <TresAmbientLight :intensity="1" />
+
+    <OrbitControls />
+    </TresCanvas>
+    Hola vue
+  </div>
+</template>
+
+<style>
+
+.tres-container{
+  width: 100%;
+  height: 400px;
+}
+
+.canvas{
+  width: 100%;
+  height: 100%;
+
+}
+
+</style>
--- src/partials/Hero.tsx
+++ src/partials/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => (
     <HeroAvatar
       title={
         <>
-          Hi there, I'm <GradientText>Ixartz</GradientText> 👋
+          Hola , I'm <GradientText>Snow Trash</GradientText> 👋
         </>
       }
       description={
